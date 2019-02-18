0f6c1d786168572c365b21015f59880db5a78b69
Pach0n <boomsixboom@gmail.com>
2019-02-18 10:51:45 +0700

    Add files via upload

--- lib/ui/first_screen.dart
+++ lib/ui/first_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import './detail_screen.dart';
 import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 import 'package:intl/intl.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class FirstScreen extends StatefulWidget{
   @override
@@ -13,18 +15,31 @@ class FirstScreen extends StatefulWidget{
 }
 class FirstScreenState extends State{
   List<String> _passengers = <String>["","1","2","3","4","5"];
+  File _image;
   String _passenger = "1";
+  Future getImage() async{
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+          _image = image;
+        });
+  }
  @override
   Widget build(BuildContext context){
     // TODO: implement createState
     DateTime date;
         return Scaffold(
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add_a_photo),
+            onPressed: getImage,
+          ),
           appBar: AppBar(
             title:  Text("First Screen"),
           ),
           body: Form(
             child: ListView(
               children: <Widget>[
+                _image == null ? Text("No images selected"): Image.file(_image),
                 InputDecorator(
                   decoration: InputDecoration(
                     icon: Icon(Icons.people),
@@ -54,10 +69,10 @@ class FirstScreenState extends State{
                   initialDate: DateTime.now(),
                   format: DateFormat("yyyy-mm-dd"),
                   decoration: InputDecoration(labelText: "Date"),
-            ),
+            )
           ],
           ),
-      )
+      ),
     );
   }
 }
